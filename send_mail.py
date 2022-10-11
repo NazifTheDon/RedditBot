bb60d2b3267c2c86661b41030ffc9f997cdbce6c
NazifTheDon <70698290+NazifTheDon@users.noreply.github.com>
2022-10-11 09:10:40 +0200

    initial commit

---
+++ send_mail.py
@@ -0,0 +1,36 @@
+import smtplib, ssl
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+
+def send_email(subject, body):
+    sender_email = "testadennaaccen@gmail.com"
+    receiver_email = "testadennaaccen@gmail.com"
+
+
+
+    content = f"""
+        {body}
+        """
+    msg = MIMEMultipart()
+
+
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = subject
+    body = MIMEText(content, "plain")
+    msg.attach(body)
+
+    filename = "test.csv"
+    with open(filename, "r", encoding="utf8") as f:
+        attachment = MIMEApplication(f.read(), Name=basename(filename))
+        attachment["Content-Dispositiob"] = 'attachment; filename="{}"'.format(basename(filename))
+    msg.attach(attachment)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, "etafpuxedtlpknug")
+        server.send_message(msg, from_addr=sender_email, to_addrs=[receiver_email])
