c99033caf34968f13147616eca019775665a3f26
nazifthedon <nazif.kadiroglu1@gmail.com>
2022-12-29 20:10:49 -0800

    You can now subscribe, unsubscribe, send email

--- app.py
+++ app.py
@@ -1,46 +1,56 @@
-from datetime import date
-
-from flask import Flask, request, render_template
-from pymongo import MongoClient
+from config import pw_database
 import re
-from main import getInfo, headers
+from main import headers
+from send_mail import send_email
 import pandas as pd
 import requests
-import logging
-
+import mysql.connector
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-
-# Connect to the MongoDB server
-client = MongoClient('mongodb://NazifTheDon:Aareyuok.123@localhost:5000/emails')
-
-# Select the database and collection to use
-db = client['emails']
-collection = db['emails']
-
-
+# Define the route for the index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Define the route for handling form submissions
-import re
 
+# Define the route for handling form submissions from the index page
 @app.route('/submit', methods=['POST'])
 def submit():
     # Check if the email field is present in the form submission
     if 'email' in request.form:
         # Get the email from the form submission
         email = request.form['email']
+        cnx = mysql.connector.connect(user='root', password=pw_database, host='localhost', database='emails')
+
+        # Create a cursor object
+        cursor = cnx.cursor()
 
         # Define the pattern for matching email addresses
         pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
         # Check if the email matches the pattern
         if re.match(pattern, email):
-            # Save the email to the database
-            collection.insert_one({'email': email})
-            return 'Email saved successfully'
+            # Check if the email is already in the database
+            sql = 'SELECT email FROM emails WHERE email = %s'
+            val = (email,)
+            cursor.execute(sql, val)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+            if len(rows) > 0:
+                # Email is already in the database
+                cursor.close()
+                cnx.close()
+                return 'You are already subscribed!'
+            else:
+                # Save the email to the MySQL database
+                sql = 'INSERT INTO emails (email) VALUES (%s)'
+                cursor.execute(sql, val)
+                cnx.commit()
+                cursor.close()
+                cnx.close()
+                return 'Email saved successfully'
         else:
             # Return an error if the email is not a valid email address
             return 'Error: not a valid email'
@@ -48,52 +58,95 @@ def submit():
         # Return an error if the email field is not found
         return 'Error: email field not found in form submission'
 
+# Define the route for the base page
 @app.route('/base.html')
 def base():
     return render_template('base.html')
 
+
+# Define the route for handling form submissions from the base page
 @app.route('/submit_now', methods=['POST'])
-def send_email():
+def send_email_now():
     email = request.form['email']
     df = pd.DataFrame()
-    link = "r/wallstreetbets/new"
-    after = None
-    while True:
-        # Make an API call to retrieve a page of results
-        params = {"limit": "100"}
-        if after:
-            params["after"] = after
-        res = requests.get(f"https://oauth.reddit.com/{link}",
-                           headers=headers, params=params)
-        # Append the relevant information about the posts to the DataFrame
-        posts = []
-        for post in res.json()["data"]["children"]:
-            data_of = post["data"]
-            description_lower = data_of["selftext"].lower()
-            title_lower = data_of["title"].lower()
-            if (" tesla " in description_lower or " tesla " in title_lower or ("tesla ") in title_lower):
-                posts.append({
-                    "Title": data_of["title"],
-                    "Abbreddit": data_of["subreddit"],
-                    "Author": data_of["author"],
-                    "Content": (data_of["selftext"] if (data_of["selftext"]) else "No Description"),
-                    "Link": data_of["url"]
-                })
-        if not posts:
-            break
-        df = df.append(posts, ignore_index=True)
-        after = res.json()["data"]["after"]
-    if df.empty:
-        return 'There are currently no post in that thread about Tesla :('
-    # Convert the DataFrame to a CSV file
-    csv = df.to_csv(index=False)
-    # Send the email with the CSV file to each email in the list
-    todays_date = date.today().strftime("%d/%m/%Y")
-    subject = "Update on reddit"
-    message = f"\nGoodmorning Nazif!\nHere is the daily update.\nTodays date is: {todays_date} \n\n- From CertifiedSideBoi (Reddit)"
-    send_email(subject, message, "info.csv", email, attachments=[("info.csv", "text/csv", csv)])
-    return "Email sent!"
+    link = "r/wallstreetbets/top?sort=new&t=day&limit=100"
+    # Make an API call to retrieve the top 100 posts
+    res = requests.get(f"https://oauth.reddit.com/{link}", headers=headers)
+    # Append the relevant information about the posts to the DataFrame
+    posts = []
+    for post in res.json()["data"]["children"]:
+        data_of = post["data"]
+        description_lower = data_of["selftext"].lower()
+        title_lower = data_of["title"].lower()
+        if "tesla" in description_lower or "tesla" in title_lower:
+            content = data_of["selftext"].replace("&amp;#x200B;", "")
+            posts.append({
+                "Title": data_of["title"],
+                "Subreddit": data_of["subreddit"],
+                "Author": data_of["author"],
+                "Content":  content
+            })
+
+    df = df.append(posts, ignore_index=True)
+    # Process the data and send the email
+    send(email, df)
+    return 'Email sent successfully'
+
+# Define the route for the unsubscribe page
+@app.route('/unsubscribe.html')
+def unsubscribe():
+    return render_template('unsubscribe.html')
+
+# Define the route for handling form submissions from the unsubscribe page
+@app.route('/unsubscribe', methods=['POST'])
+def remove_email():
+    # Check if the email field is present in the form submission
+    if 'email' in request.form:
+        # Get the email from the form submission
+        email = request.form['email']
+
+        # Connect to the MySQL database
+        cnx = mysql.connector.connect(user='root', password=pw_database, host='localhost', database='emails')
+
+        # Create a cursor object
+        cursor = cnx.cursor()
+
+        # Define the DELETE query
+        sql = 'DELETE FROM emails WHERE email = %s'
+        val = (email,)
+
+        # Execute the DELETE query
+        cursor.execute(sql, val)
+        cnx.commit()
+
+        # Check if the email was deleted
+        cursor.execute(sql, val)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        if len(rows) == 0:
+            # Email was deleted
+            cursor.close()
+            cnx.close()
+            return 'Email removed successfully'
+        else:
+            # Email was not deleted
+            cursor.close()
+            cnx.close()
+            return 'Error: email not found in database'
+    else:
+        # Return an error if the email field is not found
+        return 'Error: email field not found in form submission'
+
+def send(email, df):
+    df.to_csv("info.csv", index=False, encoding='utf-8')  # Save the data in df to a CSV file
+    subject = "Tesla updates"
+    body = f"Here are the latest Tesla updates:"
+    file = "info.csv"  # Attach the CSV file
+    attachments = None
+    send_email(subject, body, file, email, attachments)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 import pandas as pd
-from datetime import date, datetime, time
+from datetime import date, time
 import logging
 from requests.auth import HTTPBasicAuth
 from send_mail import send_email
-from pymongo import MongoClient
-from config import CLIENT_ID, SECRET_KEY, pw
-import schedule
+from config import CLIENT_ID, SECRET_KEY, pw_database, pw
+import mysql.connector
+from apscheduler.schedulers.background import BackgroundScheduler
 
 auth = HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
@@ -24,88 +24,68 @@ TOKEN = res.json()['access_token']
 headers["Authorization"] = f'bearer {TOKEN}'
 
 def main():
-    # Schedule the send_emails function to run every day at 8:00 AM
-    schedule.every().day.at("8:00").do(send_emails)
-
-def send_emails():
-    # Connect to the MongoDB server and select the database and collection
-    client = MongoClient('mongodb://NazifTheDon:Aareyuok.123@localhost:5000/emails')
-    db = client['emails']
-    collection = db['emails']
-
-    # Get a list of all emails in the database
-    emails = collection.find()
-
-    # Retrieve posts about Tesla from the subreddit r/wallstreetbets/new
-    df = pd.DataFrame()
-    link = "r/wallstreetbets/new"
-    after = None
-    while True:
-        # Make an API call to retrieve a page of results
-        params = {"limit": "100"}
-        if after:
-            params["after"] = after
-        res = requests.get(f"https://oauth.reddit.com/{link}",
-                     headers=headers, params=params)
-        # Append the relevant information about the posts to the DataFrame
-        posts = []
-        for post in res.json()["data"]["children"]:
-            data_of = post["data"]
-            description_lower = data_of["selftext"].lower()
-            title_lower = data_of["title"].lower()
-            if(" tesla " in description_lower or " tesla " in title_lower or ("tesla ") in title_lower ):
-                posts.append({
-                    "Title" : data_of["title"],
-                    "Abbreddit" : data_of["subreddit"],
-                    "Author": data_of["author"],
-                                    "Content": (data_of["selftext"] if(data_of["selftext"]) else "No Description"),
-                    "Link": data_of["url"]
-                    })
-        if not posts:
-            break
-        df = df.append(posts, ignore_index=True)
-        after = res.json()["data"]["after"]
-    if df.empty:
-        logger.info("No posts about Tesla found.")
-        return
-    # Convert the DataFrame to a CSV file
-    csv = df.to_csv(index=False)
-    # Send the email with the CSV file to each email in the list
-    todays_date = date.today().strftime("%d/%m/%Y")
-    subject = "Update on reddit"
-    message = f"\nGoodmorning Nazif!\nHere is the daily update.\nTodays date is: {todays_date} \n\n- From CertifiedSideBoi (Reddit)"
-    for email in emails:
-        send_email(subject, message, "info.csv", email, attachments=[("info.csv", "text/csv", csv)])
-
-def getInfo(link, df):
-    res = requests.get(f"https://oauth.reddit.com/{link}",
-                 headers=headers, params={"limit": "100"})
-    for post in res.json()["data"]["children"]:
-        data_of = post["data"]
-        description_lower = data_of["selftext"].lower()
-        title_lower = data_of["title"].lower()
-        if(" tesla " in description_lower or " tesla " in title_lower or ("tesla ") in title_lower ):
-            df = df.append({
-                "Title" : data_of["title"],
-                "Abbreddit" : data_of["subreddit"],
-                "Author": data_of["author"],
-                "Content": (data_of["selftext"] if(data_of["selftext"]) else "No Description"),
-                "Link": data_of["url"]
-                }, ignore_index = True)
-    csv = df.to_csv
-    return csv
-
-
-if __name__ == '__main__':
     # Set up logging
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
 
-    try:
-        main()
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
-    except Exception as e:
-        logger.exception("An error occurred:")
+    # Create a background scheduler
+    scheduler = BackgroundScheduler()
+
+    # Schedule the send_emails function to run every day at 8:00 AM
+    scheduler.add_job(send_emails, 'cron', hour=19, minute=59, timezone='US/Pacific')
+
+    # Start the scheduler
+    scheduler.start()
+def send_emails():
+    # Connect to the MySQL database
+    cnx = mysql.connector.connect(user='root', password=pw_database, host='localhost', database='emails')
+
+    # Create a cursor object
+    cursor = cnx.cursor()
+
+    # Retrieve all emails from the emails table
+    sql = 'SELECT email FROM emails'
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    emails = [row[0] for row in rows]
+    print("working")
+    # Retrieve posts about Tesla from the subreddit r/wallstreetbets/new
+    df = pd.DataFrame()
+    link = "r/wallstreetbets/top?sort=new&t=day&limit=100"
+    # Make an API call to retrieve the top 100 posts
+    res = requests.get(f"https://oauth.reddit.com/{link}", headers=headers)
+    # Append the relevant information about the posts to the DataFrame
+    posts = []
+    for post in res.json()["data"]["children"]:
+        data_of = post["data"]
+        description_lower = data_of["selftext"].lower()
+        title_lower = data_of["title"].lower()
+        if "tesla" in description_lower or "tesla" in title_lower:
+            content = data_of["selftext"].replace("&amp;#x200B;", "")
+            posts.append({
+                "Title": data_of["title"],
+                "Subreddit": data_of["subreddit"],
+                "Author": data_of["author"],
+                "Content": content
+            })
+
+    df = df.append(posts, ignore_index=True)
+
+    # Process the data and send the email
+    # Convert the DataFrame to a CSV file
+    csv = df.to_csv("info.csv", index=False)
+    # Send the email with the CSV file to each email in the list
+    todays_date = date.today().strftime("%d/%m/%Y")
+    subject = "Update on reddit"
+    message = f"\nGoodmorning!\nHere is the daily update.\nTodays date is: {todays_date} \n\n- From CertifiedSideBoi (Reddit)"
+    for email in emails:
+        send_email(subject, message, "info.csv", email, attachments=[("info.csv", "text/csv", csv)])
+
+    # Close the cursor and connection to the database
+    cursor.close()
+    cnx.close()
+
+
+if __name__ == '__main__':
+    main()
